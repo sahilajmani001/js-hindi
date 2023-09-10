d7a40d458dea9ddb96e08fd8d3ed8476baf6e1b9
sahilajmani <35402715+sahilajmani001@users.noreply.github.com>
2023-09-10 23:33:11 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sahilajmani@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+
+//accountId = 2
+
+accountEmail = "sash@gmail.com"
+accountPassword = "567890"
+accountCity = "nagpur"
+
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword])
---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version 
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+
+
+
+// object
+
+console.log(typeof undefined); // output - undefined
+console.log(typeof null) //object 
